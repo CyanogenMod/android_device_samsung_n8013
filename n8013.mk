994d5eaeb5b45c3a21979b2ee5948274f0c1ab1d
lirokoa <lirokoa@hotmail.com>
2015-01-13 18:09:43 +0100

    tiny_hw.xml is now copied in common repo

--- n8013.mk
+++ n8013.mk
@@ -23,9 +23,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_COPY_FILES += \
     device/samsung/n8013/rootdir/init.target.rc:root/init.target.rc
 
-# Audio
-PRODUCT_COPY_FILES += \
-    device/samsung/n80xx-common/configs/tiny_hw.xml:system/etc/sound/GT-N8013
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
